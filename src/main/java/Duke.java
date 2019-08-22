150ce8ee9dccc22a8a81959994398b3b2bcf6c92
uberSaiyan <yanwl.note@gmail.com>
2019-08-22 13:40:02 +0800

    Added greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         Duke duke = new Duke();
-        duke.greet();
+        prettyPrint(duke.greetings());
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            String[] parameters = input.split(" ");
+            String command = parameters[0];
+
+            switch (command) {
+            case "bye":
+                prettyPrint(duke.goodbye());
+                System.exit(0);
+                break;
+
+            default:
+                prettyPrint(duke.echo(command));
+            }
+        }
     }
 
-    public void greet() {
-        String greetings = "";
-        greetings += "    ____________________________________________________________\n";
-        greetings += "     Hello! I'm Duke\n";
-        greetings += "     What can I do for you?\n";
-        greetings += "    ____________________________________________________________\n";
-        System.out.print(greetings);
+    private static void prettyPrint(String[] messages) {
+        String indentation = "    ";
+        String linebreak = "____________________________________________________________";
+        System.out.println(indentation + linebreak);
+        for (String msg : messages) {
+            System.out.println(indentation + " " + msg);
+        }
+        System.out.println(indentation + linebreak);
+    }
+
+    public String[] greetings() {
+        String[] greetings = {"Hello, I'm Duke", "What can I do for you?"};
+        return greetings;
+    }
+
+    public String[] goodbye() {
+        String[] goodbye = {"Bye. Hope to see you again soon!"};
+        return goodbye;
+    }
+
+    public String[] echo(String input) {
+        String[] echo = {input};
+        return echo;
     }
 }
