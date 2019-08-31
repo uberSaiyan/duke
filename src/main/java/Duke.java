00a5cbca56c90f93148a08cbd0d83dd3f37701d2
uberSaiyan <yanwl.note@gmail.com>
2019-08-31 14:28:49 +0800

    Added basic GUI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,12 +5,8 @@ import duke.Ui;
 import duke.Parser;
 import duke.command.Command;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+public class Duke {
 
-public class Duke extends Application {
     private Storage storage;
     private TaskList taskList;
     private Ui ui;
@@ -50,12 +46,11 @@ public class Duke extends Application {
         new Duke("data/tasks.txt").run();
     }
 
-    @Override
-    public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our screen
-        stage.show(); // Render the stage.
+    /**
+     * You should have your own function to generate a response to user input.
+     * Replace this stub with your completed method.
+     */
+    String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
