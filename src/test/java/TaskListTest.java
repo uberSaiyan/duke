b1fb5183dee2727e3162424f8eb2df6ed0ef3d7a
uberSaiyan <yanwl.note@gmail.com>
2019-09-16 18:15:10 +0800

    Fix checkstyle

--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -8,8 +8,8 @@ public class TaskListTest {
     public void select_multiplePredicates_returnFiltered() {
         TaskList taskList = new TaskListStub();
         TaskList filteredTaskList = taskList.select(
-                task -> task.getDescription().length() < 45,
-                task -> task.getDescription().contains(",")
+            task -> task.getDescription().length() < 45,
+            task -> task.getDescription().contains(",")
         );
         assertEquals(filteredTaskList.size(), 2);
     }
